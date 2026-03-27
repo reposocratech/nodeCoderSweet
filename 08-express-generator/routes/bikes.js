a9752a3cb9857167ede47cd6d902aaae5e2e9023
Santi <cvteatinos@hotmail.com>
2026-03-27 08:22:49 +0100

    repaso

--- 08-express-generator/routes/bikes.js
+++ 08-express-generator/routes/bikes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../config/db')
 
+
 //sirve la página que muestra el catálogo de bicibletas
 //URL: http://localhost:4000/bikes/allBikes
+
 
 router.get("/allBikes", (req, res)=>{
     //llamad a base de datos
     let sql = 'SELECT * FROM bike'
-
     connection.query(sql, (error, result)=>{
         if(error){
             throw error;

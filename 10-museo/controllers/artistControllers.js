41e9c7a82c98ec17305041eb6a2069bc6c91794a
Santi <santiago.pena@socratech.es>
2026-04-08 15:06:32 +0200

    artist crud

---
+++ 10-museo/controllers/artistControllers.js
@@ -0,0 +1,114 @@
+const bcrypt = require("bcrypt");
+
+const connection = require("../config/db");
+
+class ArtistController {
+  showRegister = (req, res) => {
+    // res.render("register", { message:"" });
+
+    res.render("register");
+  };
+
+  register = (req, res) => {
+    const { name, lastname, email, password } = req.body;
+
+    //validación de que todos los campos estén rellenos
+    if(!name || !lastname || !email || !password){
+        res.render('register', {message: "Debes cumplimentar todos los campos"})
+    }else{
+        //encriptar la password
+        bcrypt.hash(password, 10, (errHash, hashedPassword) => {
+            if (errHash) {
+                throw errHash;
+            } else {
+                console.log("************************************", hashedPassword);
+                let sql =
+                "INSERT INTO artist (name, lastname, email, password) VALUES (?,?,?,?)";
+                let values = [name.trim(), lastname.trim(), email.trim(), hashedPassword.trim()];
+                
+                connection.query(sql, values, (err, result) => {
+                    if (err) {
+                        if(err.errno == 1062){
+                            res.render('register', {message:"Email duplicado"})
+                        }else{
+                            throw err;
+                        }            
+                    } else {
+                        res.redirect("/artist/login");
+                    }
+                });
+            }
+        });
+    }
+  };
+
+  showLogin = (req, res) => {
+    res.render("login");
+  };
+
+  login = (req, res) => {
+    const email = req.body.email.trim();
+    const password = req.body.password.trim();
+    //validaciones
+    if(!email || !password){
+        res.render('login', {message: "Debas cumplimentar todos los campos"})
+    }else{
+        //1º ver si el artista existe
+        let sql = 'SELECT * FROM artist WHERE email = ? AND artist_is_deleted = 0';
+        connection.query(sql, [email], (err, result)=>{
+            if(err){
+                throw err;
+            }else{
+                if(!result.length){
+                    res.render('login', {message:"Credenciales incorrectas"})
+                }else{
+                    //ver si la contraseña es la adecuada para ese artista
+                    //comprobar que la contraseña que viene con el formulario 
+                    // equivale a la que está en la base de datos
+                    let hashedPass = result[0].password;
+                    bcrypt.compare(password, hashedPass, (errCompare, resultCompare)=>{
+                        if(errCompare){
+                            throw errCompare
+                        }else{
+                            if(resultCompare===true){
+                                res.redirect(`/artist/profile/${result[0].artist_id}`)                    
+                            }else{
+                                res.render("login", {message: "Credenciales incorrectas"})
+                            }
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+  }
+
+  allArtist = (req, res) => {
+    let sql = 'SELECT * FROM artist WHERE artist_is_deleted = 0'
+    connection.query(sql, (err, result)=>{
+        if(err){
+            throw err;
+        }else{
+            console.log("2222",result);
+            
+            res.render("allArtists", {artists: result});
+        }
+    })
+  };
+
+  profile = (req, res) => {
+    const { id } = req.params;
+    let sql = 'SELECT * FROM artist WHERE artist_id = ? AND artist_is_deleted = 0';
+    connection.query(sql, [id], (err, result)=>{
+        if(err){
+            throw err
+        }else{     
+            res.render('profile', {artista:result[0]});
+        }
+    })
+  }
+
+}
+
+module.exports = new ArtistController();
--- 10-museo/routes/artist.js
+++ 10-museo/routes/artist.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const artistController = require('../controllers/artistControllers')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//1.- abre el formulario de registro de artista
+//url: http://localhost:4000/artist/register
+router.get('/register', artistController.showRegister);
+
+//2.- recoge los datos del formulario de registro y registra el artista
+//url: http://localhost:4000/artist/register
+router.post('/register', artistController.register);
+
+//3.- abre el formulario de login de artista
+//url: http://localhost:4000/artist/login
+router.get('/login', artistController.showLogin);
+
+//4.- recoge los datos del form login y haga el login del artista
+//url: http://localhost:4000/artist/login
+router.post('/login', artistController.login)
+
+//5.- nos muestra todos los artistas
+//url: http://localhost:4000/artist/allArtist
+router.get('/allArtist', artistController.allArtist);
+
+//6.- nos muestra el profile de un artista
+//url: http://localhost:4000/artist/profile/56
+router.get('/profile/:id', artistController.profile)
 
 module.exports = router;
